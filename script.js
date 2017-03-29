2c50b0a806d64652c9271a01f2542966647f42d0
carolinedeng <caroline.m.deng@gmail.com>
2017-03-29 18:07:40 -0400

    edited css and html to make the header image a background image + some more custom css around horizontal lines

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var images = ["Skeleton/images/DSC00759.JPG", "Skeleton/images/DSC00763.JPG", "Skeleton/images/DSC00896.JPG", 
-"Skeleton/images/DSC01169.JPG", "Skeleton/images/DSC01184.JPG", "Skeleton/images/DSC01189.JPG", "Skeleton/images/DSC01193.JPG",
-"Skeleton/images/DSC01194.JPG", "Skeleton/images/DSC01203.JPG"];
-
-var captions = ["caption1 --> 9","2", "3", "4", "5", "6", "7", "8", "9"]
+var images = [
+{src:"Skeleton/images/DSC00759.JPG", caption: "wow"},
+{src:"Skeleton/images/DSC00763.JPG", caption: "cool"},
+{src:"Skeleton/images/DSC00896.JPG", caption: "sweet"},
+{src:"Skeleton/images/DSC01169.JPG", caption: "dope"},
+{src:"Skeleton/images/DSC01184.JPG", caption: "hello"},
+{src:"Skeleton/images/DSC01189.JPG", caption: "test"},
+{src:"Skeleton/images/DSC01193.JPG", caption: "caption!"},
+{src:"Skeleton/images/DSC01194.JPG", caption: "yum"},
+{src:"Skeleton/images/DSC01203.JPG", caption: "yay"}];
 
 var imageIndex = 0;
 var rightarrow = document.querySelector(".rightarrow");
@@ -11,22 +16,22 @@ var leftarrow = document.querySelector(".leftarrow");
 function changeImage() {
   imageIndex ++;
   if (imageIndex > images.length - 1) imageIndex = 0;
-  $('#slideshowImage').attr('src', images[imageIndex]);
-  $('.caption').html(captions[imageIndex]);
+  $('#slideshowImage').attr('src', images[imageIndex].src);
+  $('.caption').html(images[imageIndex].caption);
 };
 
 function nextImage() {
   imageIndex ++;
   if (imageIndex > images.length - 1) imageIndex = 0;
-  $('#slideshowImage').attr('src', images[imageIndex]);
-  $('.caption').html(captions[imageIndex]);
+  $('#slideshowImage').attr('src',images[imageIndex].src);
+  $('.caption').html(images[imageIndex].caption);
 };
 
 function previousImage() {
     imageIndex --;
     if (imageIndex < 0) imageIndex = images.length-1;
-    $('#slideshowImage').attr('src', images[imageIndex]);
-  	$('.caption').html(captions[imageIndex]);
+    $('#slideshowImage').attr('src',images[imageIndex].src);
+  	$('.caption').html(images[imageIndex].caption);
 }
 
 leftarrow.addEventListener("click", nextImage);
