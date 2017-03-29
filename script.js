93b530e48697c870109dd644d32b6241ddff5f9f
carolinedeng <caroline.m.deng@gmail.com>
2017-03-29 17:36:12 -0400

    Created custom.css and fixed image gallery

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var images = ["Skeleton/images/DSC00759.JPG", "Skeleton/images/DSC00763.JPG", "S
 "Skeleton/images/DSC01169.JPG", "Skeleton/images/DSC01184.JPG", "Skeleton/images/DSC01189.JPG", "Skeleton/images/DSC01193.JPG",
 "Skeleton/images/DSC01194.JPG", "Skeleton/images/DSC01203.JPG"];
 
+var captions = ["caption1 --> 9","2", "3", "4", "5", "6", "7", "8", "9"]
+
 var imageIndex = 0;
 var rightarrow = document.querySelector(".rightarrow");
 var leftarrow = document.querySelector(".leftarrow");
@@ -10,18 +12,21 @@ function changeImage() {
   imageIndex ++;
   if (imageIndex > images.length - 1) imageIndex = 0;
   $('#slideshowImage').attr('src', images[imageIndex]);
+  $('.caption').html(captions[imageIndex]);
 };
 
 function nextImage() {
   imageIndex ++;
   if (imageIndex > images.length - 1) imageIndex = 0;
   $('#slideshowImage').attr('src', images[imageIndex]);
+  $('.caption').html(captions[imageIndex]);
 };
 
 function previousImage() {
     imageIndex --;
     if (imageIndex < 0) imageIndex = images.length-1;
     $('#slideshowImage').attr('src', images[imageIndex]);
+  	$('.caption').html(captions[imageIndex]);
 }
 
 leftarrow.addEventListener("click", nextImage);
