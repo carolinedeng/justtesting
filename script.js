8bdc1c391e5bc350f883026f363298f7990a4700
carolinedeng <caroline.m.deng@gmail.com>
2017-04-02 00:04:37 -0400

    Updated eat+drink html to be consistent with index
    Added footer, changed Lorem Ipsum text, added some CSS, edited the
    JQuery image gallery

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 var images = [
-{src:"Skeleton/images/DSC00759.JPG", caption: "wow"},
-{src:"Skeleton/images/DSC00763.JPG", caption: "cool"},
-{src:"Skeleton/images/DSC00896.JPG", caption: "sweet"},
-{src:"Skeleton/images/DSC01169.JPG", caption: "dope"},
-{src:"Skeleton/images/DSC01184.JPG", caption: "hello"},
-{src:"Skeleton/images/DSC01189.JPG", caption: "test"},
-{src:"Skeleton/images/DSC01193.JPG", caption: "caption!"},
-{src:"Skeleton/images/DSC01194.JPG", caption: "yum"},
-{src:"Skeleton/images/DSC01203.JPG", caption: "yay"}];
+{src:"Skeleton/images/DSC00759.JPG", caption: "<strong>Takoyaki</strong><br /><br />A popular, ball-shaped Japanese snack made out of flower and filled with octopus, tempura scraps, ginger, and scallion."},
+{src:"Skeleton/images/DSC00763.JPG", caption: "<strong>Okonomiyaki</strong> <br /><br />A Japanese savory pancake containing a variety of ingredients, including flour, eggs, shredded cabbage, meat, and some condiments of your choice."},
+{src:"Skeleton/images/DSC00896.JPG", caption: "<strong>Ramen</strong><br /><br />Slightly thin, curly noodles served in a soy-flavored chicken broth and with various toppings."},
+{src:"Skeleton/images/DSC01194.JPG", caption: "<strong>Sushi</strong><br /><br />A mixture of vinegared rice, seaweed, and ingredients such as seafood, vegetables, meat, and sometimes, tropical fruits."},
+{src:"Skeleton/images/DSC01203.JPG", caption: "<strong>Mochi</strong><br /><br />A rice cake in the shape of a ball that is pounded into paste. It can be filled with ingredients like red bean, matcha, strawberry, and more."}];
 
 var imageIndex = 0;
 var rightarrow = document.querySelector(".rightarrow");
@@ -37,4 +33,4 @@ function previousImage() {
 leftarrow.addEventListener("click", nextImage);
 rightarrow.addEventListener("click", previousImage);
 
-setInterval(changeImage, 4000);
+setInterval(changeImage, 6000);
